1f3e8231c6196fca4896457c1b4ca45f2cac81b3
faishalbasbeth <faishalbasbeth@gmail.com>
2020-02-09 01:27:18 +0700

    send params detail

--- src/container/pages/BlogPost/DetailPost/DetailPost.jsx
+++ src/container/pages/BlogPost/DetailPost/DetailPost.jsx
@@ -1,25 +1,33 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
+import './DetailPost.css';
 
 class DetailPost extends Component {
     state = {
         post: {
-            title="",
-            title=""
+            title:'',
+            body:''
         }
     }
     componentDidMount(){
         let id = this.props.match.params.postID;
         Axios.get(`http://localhost:3004/posts/${id}`).then(res => {
             console.log('result', res);
+            let post = res.data;
+            this.setState({
+                post : {
+                    title : post.title,
+                    body : post.body
+                }
+            })
         })
 
     }
     render(){
         return(
-            <div>
-                <p>{this.state.post.title}</p>
-                <p>{this.state.post.body}</p>
+            <div className="p-detail-post">
+                <p className="detail-title">{this.state.post.title}</p>
+                <p className="detail-body">{this.state.post.body}</p>
             </div>
         )
     }
